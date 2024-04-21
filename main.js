6c08b61fbe696cd7ee3b3f94d2ef0262623f70ac
DELL <afridi2542@gmail.com>
2024-04-21 18:37:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightINKG = 70;
+let heightINmeter = 1.75;
+let bmi = weightINKG / heightINmeter * heightINmeter;
+console.log(`Your bmi is ${bmi}`);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let weightINKG = 70;
+
+let heightINmeter = 1.75;
+let bmi = weightINKG / heightINmeter*heightINmeter;
+
+console.log(`Your bmi is ${bmi}`);
